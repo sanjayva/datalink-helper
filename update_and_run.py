0dfd942ba4eeb9efab80722e7e0996626246e9de
sanjayva <sanjayva@users.noreply.github.com>
2018-11-07 17:40:13 -0800

    Updated functions for presentaion

--- update_and_run.py
+++ update_and_run.py
@@ -1,19 +1,28 @@
 # -*- coding: utf-8 -*-
-    
+
+#########################################################
+#
+#  Datalink API Framework
+#
+#  Author: Sanjay Valiyaveettil
+#
+#
+#########################################################
+  
 import requests
-import json
-import urllib
 import time
+import json
 
 ##### Declarations go here
 FRONTDOOR_URL = 'https://frontdoor.apptio.com/service/apikeylogin'
-APIPUBLICKEY = 'PLEASE_ADD_THIS'
-APISECRETKEY = 'PLEASE_ADD_THIS'
-BASEURL4DATALINK = 'https://CUSTOMER-manager.apptio.com' #<< Please update this too
-agent_id = 'PLEASE_ADD_THIS'
+APIPUBLICKEY = ''
+APISECRETKEY = ''
+BASEURL4DATALINK = 'https://dl-apptiotr-tbmc-manager.apptio.com'
+agent_id = ''
 
 ##### Public Variables go here
 auth_token = ''
+
 
 ##### Function definitions
 def get_auth_token():
@@ -23,17 +32,18 @@ def get_auth_token():
     
     data = {"keyAccess": APIPUBLICKEY, "keySecret": APISECRETKEY}
     resp = requests.post(FRONTDOOR_URL, headers={"Content-Type": "application/json"}, json=data)
-    
+
     if resp.status_code == 200:
         auth_token = resp.headers['apptio-opentoken']
+        
     else:
         raise ValueError("could not authenticate, got response: " + str(resp.status_code) + ": " + resp.json()["error"])
 
-def get_connector_config():
+def get_connector_config(connectorid):
     """Gets the connector configuration JSON for a given connector and agent."""
 
-    if auth_token == '':
-      get_auth_token()
+    get_auth_token()
+    global agent_id
     
     connector_url = BASEURL4DATALINK +  "/api/v1/resource/agent/" + agent_id + "/connector/" + connectorid
     connector_config_resp = requests.get(connector_url, headers={"Content-Type": "application/json", "apptio-opentoken": auth_token})
@@ -46,8 +56,8 @@ def get_connector_config():
 def update_connector_config(connectorid, connectorconfig):
     """Updates the connector configuration for a given connector ID and agent."""
 
-    if auth_token == '':
-      get_auth_token()
+    get_auth_token()
+    global agent_id
     
     connector_url = BASEURL4DATALINK +  "/api/v1/resource/agent/" + agent_id + "/connector/" + connectorid
     connector_config_resp = requests.put(connector_url, headers={"Content-Type": "application/json", "apptio-opentoken": auth_token}, json=connectorconfig)
@@ -61,21 +71,21 @@ def update_connector_config(connectorid, connectorconfig):
 def get_connector_status(connectorid):
     """Gets the connector status for a given connector and agent."""
 
-    if auth_token == '':
-      get_auth_token()
+    get_auth_token()
+    global agent_id
     
     connector_url = BASEURL4DATALINK +  "/api/v1/resource/agent/" + agent_id + "/connector/" + connectorid + "/status"
     connector_config_resp = requests.get(connector_url, headers={"Content-Type": "application/json", "apptio-opentoken": auth_token})
     if connector_config_resp.status_code == 200:
-        return connector_config_resp.json()['status']
+        return connector_config_resp.json()['statusMessage']
     else:
         raise ValueError("Could not get the connector status, got response: " + str(connector_config_resp.status_code) + ": " + connector_config_resp.json()["error"])
 
 def kick_off_connector(connectorid):
     """Kicks off the connector for a given agent."""
 
-    if auth_token == '':
-      get_auth_token()
+    get_auth_token()
+    global agent_id
     
     connector_url = BASEURL4DATALINK +  "/api/v1/resource/agent/" + agent_id + "/connector/" + connectorid + "/execute"
     data = {"task": "ExecuteConnector"}
@@ -90,8 +100,8 @@ def kick_off_connector(connectorid):
 def get_connector_list():
     """Gets the list of connectors attached to an agent"""
 
-    if auth_token == '':
-      get_auth_token()
+    get_auth_token()
+    global agent_id
     
     connector_list_url = BASEURL4DATALINK +  "/api/v1/resource/agent/" + agent_id + "/connector?include=name,type,lastRun,status"
     connector_list_resp = requests.get(connector_list_url, headers={"Content-Type": "application/json", "apptio-opentoken": auth_token})
@@ -119,16 +129,27 @@ def get_agent_details(agentid):
     if auth_token == '':
       get_auth_token()
     
+    global agent_id
+    
     agent_details_url = BASEURL4DATALINK +  "/api/v1/resource/agent/" + agentid
     agent_details_resp = requests.get(agent_details_url, headers={"Content-Type": "application/json", "apptio-opentoken": auth_token})
     if agent_details_resp.status_code == 200:
         return agent_details_resp.json()
     else:
         raise ValueError("Could not retrieve the agent list, got response: " + str(agent_details_resp.status_code) + ": " + agent_details_resp.json()["error"])
-        
+
 def gotosleep(secs):
     """ Function to wait for n seconds """
     print("Sleeping for " + str(secs) + " seconds")
     time.sleep(secs)
      
+
+def prettyprint(jsontext):
+    """ Function to format json output in human readable format """
+    print(json.dumps(jsontext,sort_keys=False, indent=4, separators=(',', ': ') ))
+
+
 ##### Actual Script starts here
+
+
+
